597f6fdefe8509e3b7a898bed7857de8335ef395
Iris Z <irisz5926@gmail.com>
2022-01-27 23:06:40 -0500

    completed 5,6

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
@@ -9,7 +10,12 @@ app.get("/",function(req,res){
     res.sendFile(__dirname + "/views/index.html");
 });
 
-
+app.get("/json",function(req,res){
+    if(process.env.MESSAGE_STYLE=='uppercase'){
+        res.json({"message":"HELLO JSON"});
+    }
+    else res.json({"message":"Hello json"});
+});
 
 
 
